0f94a0437eedb3454ead2e7429de35c64b3a0211
Carrillo <acarrillo@elpuig.xeill.net>
2026-04-23 22:40:49 +0200

    primer commit

--- SCRIPTS BOTONES/selecion_de_juegos.gd
+++ SCRIPTS BOTONES/selecion_de_juegos.gd
@@ -1,16 +1,27 @@
 extends Node
 
 func _on_snake_guau_pressed():
-	get_tree().change_scene_to_file("res://JUEGOS/SNAKE GUAU/scenes/main.tscn")
+	DisplayServer.window_set_size(Vector2i(1004, 1082))
+	DisplayServer.window_set_position(Vector2i(100, 50))
+	get_tree().change_scene_to_file("res://SNAKE GUAU/scenes/main.tscn")
 
 func _on_tetris_guau_pressed():
-	get_tree().change_scene_to_file("res://JUEGOS/tetris-guau-main/scenes/tile_map.tscn")
-
-func _on_space_invaders_guau_pressed():
-	get_tree().change_scene_to_file("res://JUEGOS/TopDownShooter/scenes/game.tscn")
+	DisplayServer.window_set_size(Vector2i(850, 750))
+	DisplayServer.window_set_position(Vector2i(200, 50))
+	get_tree().root.content_scale_size = Vector2i(850, 750)
+	get_tree().change_scene_to_file("res://tetris-guau-main/scenes/tile_map.tscn")
 
 func _on_billar_guau_pressed():
-	get_tree().change_scene_to_file("res://JUEGOS/Billar-guau-main/scenes/main.tscn")
+	get_tree().change_scene_to_file("res://Billar-guau-main/scenes/main.tscn")
 
 func _on_busca_mines_guau_pressed():
-	get_tree().change_scene_to_file("res://JUEGOS/Busca-minas-guau-main/scenes/main.tscn")
+	get_tree().change_scene_to_file("res://Busca-minas-guau-main/scenes/main.tscn")
+
+func _on_pig_pong_guau_pressed() -> void:
+	pass # Replace with function body.
+
+func _on_space_inavders_guau_pressed() -> void:
+	get_tree().change_scene_to_file("res://TopDownShooter/scenes/game.tscn")
+	
+func _on_volver_al_menu_principal_pressed() -> void:
+	get_tree().change_scene_to_file("res://SCENES/menu.tscn")
--- SNAKE GUAU/scenes/main.gd
+++ SNAKE GUAU/scenes/main.gd
@@ -2,24 +2,22 @@ extends Node
 
 @export var snake_scene : PackedScene
 
-#game variables
 var score : int
 var game_started : bool = false
 
-#grid variables
-var cells_x : int = 20 
+var cells_x : int = 20
 var cells_y : int = 20
 var cell_size : int = 50
+
+var grid_offset : Vector2
 
 var food_pos : Vector2
 var regen_food : bool = true
 
-#sanke variables
 var old_data : Array
-var snake_data :Array
+var snake_data : Array
 var snake : Array
 
-#movement variables
 var start_pos = Vector2(9, 9)
 var up = Vector2(0, -1)
 var down = Vector2(0, 1)
@@ -28,14 +26,49 @@ var right = Vector2(1, 0)
 var move_direction : Vector2
 var can_move = true
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	new_game()
+	if not InputMap.has_action("move_up"):
+		InputMap.add_action("move_up")
+		var up_key = InputEventKey.new()
+		up_key.keycode = KEY_UP
+		InputMap.action_add_event("move_up", up_key)
+		var w_key = InputEventKey.new()
+		w_key.keycode = KEY_W
+		InputMap.action_add_event("move_up", w_key)
 	
+	if not InputMap.has_action("move_down"):
+		InputMap.add_action("move_down")
+		var down_key = InputEventKey.new()
+		down_key.keycode = KEY_DOWN
+		InputMap.action_add_event("move_down", down_key)
+		var s_key = InputEventKey.new()
+		s_key.keycode = KEY_S
+		InputMap.action_add_event("move_down", s_key)
+	
+	if not InputMap.has_action("move_left"):
+		InputMap.add_action("move_left")
+		var left_key = InputEventKey.new()
+		left_key.keycode = KEY_LEFT
+		InputMap.action_add_event("move_left", left_key)
+		var a_key = InputEventKey.new()
+		a_key.keycode = KEY_A
+		InputMap.action_add_event("move_left", a_key)
+	
+	if not InputMap.has_action("move_right"):
+		InputMap.add_action("move_right")
+		var right_key = InputEventKey.new()
+		right_key.keycode = KEY_RIGHT
+		InputMap.action_add_event("move_right", right_key)
+		var d_key = InputEventKey.new()
+		d_key.keycode = KEY_D
+		InputMap.action_add_event("move_right", d_key)
+	
+	grid_offset = Vector2(2, 50)
+	new_game()
+
 func new_game() -> void:
 	get_tree().paused = false 
-	get_tree().call_group("segments" , "queue_free")
+	get_tree().call_group("segments", "queue_free")
 	$GameOverMenu.hide()
 	score = 0
 	$Hud.get_node("ScoreLabel").text = "PUNTUACION: " + str(score)
@@ -43,7 +76,7 @@ func new_game() -> void:
 	can_move = true 
 	generate_snake()
 	move_food()
-	
+
 func generate_snake():
 	old_data.clear()
 	snake_data.clear()
@@ -55,7 +88,8 @@ func generate_snake():
 func add_segment(pos):
 	snake_data.append(pos)
 	var SnakeSegment = snake_scene.instantiate()
-	SnakeSegment.position = (pos * cell_size) + Vector2(0, cell_size)
+	SnakeSegment.position = grid_offset + (pos * cell_size)
+	SnakeSegment.size = Vector2(cell_size, cell_size)
 	add_child(SnakeSegment)
 	snake.append(SnakeSegment)
 
@@ -64,7 +98,6 @@ func _process(_delta):
 
 func move_snake():
 	if can_move:
-		#update movement from keypresses
 		if Input.is_action_just_pressed("move_down") and move_direction != up:
 			move_direction = down
 			can_move = false
@@ -86,23 +119,18 @@ func move_snake():
 			if not game_started:
 				start_game()
 
-
 func start_game():
 	game_started = true
 	$MoveTimer.start()
 
-
 func _on_move_timer_timeout():
-	#allow snake movement
 	can_move = true
-	#use the snake's previous position to move the segments
 	old_data = [] + snake_data
 	snake_data[0] += move_direction
 	for i in range(len(snake_data)):
-		#move all the segments along by one
 		if i > 0:
 			snake_data[i] = old_data[i - 1]
-		snake[i].position = (snake_data[i] * cell_size) + Vector2(0, cell_size)
+		snake[i].position = grid_offset + (snake_data[i] * cell_size)
 	check_out_of_bounds()
 	check_self_eaten()
 	check_food_eaten()
@@ -111,14 +139,12 @@ func check_out_of_bounds():
 	if snake_data[0].x < 0 or snake_data[0].x >= cells_x or snake_data[0].y < 0 or snake_data[0].y >= cells_y:
 		end_game()
 
-
 func check_self_eaten():
 	for i in range(1, len(snake_data)):
 		if snake_data[0] == snake_data[i]:
 			end_game()
-			
+
 func check_food_eaten():
-	#if snake eats the food, add a segment and move the food
 	if snake_data[0] == food_pos:
 		score += 1
 		$Hud.get_node("ScoreLabel").text = "PUNTUACION: " + str(score)
@@ -127,21 +153,20 @@ func check_food_eaten():
 
 func move_food():
 	var valid_positions = []
-	for x in range(1, cells_x - 1):
-		for y in range(2, cells_y - 1):
+	for x in range(0, cells_x):
+		for y in range(0, cells_y):
 			var pos = Vector2(x, y)
 			if not snake_data.has(pos):
 				valid_positions.append(pos)
 	if valid_positions.size() > 0:
 		food_pos = valid_positions[randi() % valid_positions.size()]
-		$Food.position = (food_pos * cell_size) + Vector2(0, cell_size)
-		
+		$Food.position = grid_offset + (food_pos * cell_size)
+
 func end_game():
 	$GameOverMenu.show()
 	$MoveTimer.stop()
 	game_started = false
 	get_tree().paused = true
-	
 
 func _on_game_over_menu_restart() -> void:
 	new_game()
